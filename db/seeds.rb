5dccdc25ae82b5109afc7ee0cac1fccde508f384
Josh Arce <contact@josharce.io>
2017-09-26 18:03:13 -0700

    checkpoint-26-posts: complete
    Assign posts and topics to Users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
 
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+
+users = User.all
+
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -17,6 +27,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -31,7 +42,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'josh.arce@icloud.com', # replace this with your personal email
+  password: 'bebedk12'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
